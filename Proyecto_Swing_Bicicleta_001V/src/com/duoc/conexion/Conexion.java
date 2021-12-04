99aeedfde4789f4696690a8c104229067d7c6d2e
alex baeza <alcebaeza@gmail.com>
2021-12-04 17:26:46 -0300

    conexiones listas con la bdd creada

---
+++ Proyecto_Swing_Bicicleta_001V/src/com/duoc/conexion/Conexion.java
@@ -0,0 +1,59 @@
+
+package com.duoc.conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class Conexion {
+    
+    public static Conexion instance; // Aplicar patrón SINGLETON
+    private Connection conexion;
+    private final String USER = "root";
+    private final String PASSWORD = "";
+    private final String SERVER = "localhost:3306";
+    private final String BBDD = "bbdd_empleados_001";
+    
+    private Conexion(){
+        try
+        {
+            //Lo que quiero ejecutar
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://" + SERVER + "/" + BBDD;
+            conexion = DriverManager.getConnection(url, USER, PASSWORD);    
+        }
+        catch(ClassNotFoundException | SQLException ex)
+        {
+            //Como manejaremos los errores en caso de suceda una excepcion
+            JOptionPane.showMessageDialog(null, "Problemas con la conexión a la base de datos\n"
+                        + "Contacte con el administrador:\n"
+                        + ex.getMessage(), "Mensajes", JOptionPane.ERROR_MESSAGE);
+
+        }
+     
+    }
+    
+    
+    // Conexiones sincrónicas, va por orden de llegada
+    public synchronized static Conexion obtenerConexion(){
+        if(instance == null)
+        {
+            instance = new Conexion();
+        }
+        return instance;
+      
+    }
+    
+    public Connection getConexion(){
+        return conexion;
+       
+    }
+    
+    public void cerrarConexion(){
+    
+        instance = null;
+    
+    }
+    
+}
